f4688b2f4a700ce28dd0336a9dab1ec20bd8431d
Samantha Hill <samymhill@gmail.com>
2022-06-22 20:21:35 -0700

    Errors fixed

--- product-page/src/App.tsx
+++ product-page/src/App.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import { ChakraProvider, theme } from "@chakra-ui/react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ProductPage } from "./Pages/ProductPage";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Routes>
-      <Route path="/" element={<ProductPage />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductPage />} />
+      </Routes>
+    </BrowserRouter>
   </ChakraProvider>
 );
